272dd836f1594bf9b2cc1d0ae881475d5575a3fb
Andrew59-boop <millerfaabi@gmail.com>
2020-07-13 20:02:54 +0300

    modifications

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    
+    SECRET_KEY =''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://foxx:bird@localhost/pitch'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+
+    
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+
+    DEBUG = True
+
+config_options = {
+    'development' :DevConfig,
+    'production' :ProdConfig,
+    
+}    
